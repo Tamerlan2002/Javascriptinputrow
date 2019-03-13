d02510d5125dadc8ea773be297352eaf15cdd07d
Tamerlan2002 <46812872+Tamerlan2002@users.noreply.github.com>
2019-03-14 00:15:25 +0400

    Add files via upload

---
+++ Javascripts/script.js
@@ -0,0 +1,14 @@
+function myFunction(){
+    let importedNum = document.getElementById("inputRow").value;
+    let numRow = document.getElementsByTagName("P")[importedNum].style.color = 'red';
+
+
+    //Sehvimin harda oldugunu deseydiniz sevinderdim))
+    // if (typeof (importedNum) == Number){
+    //     return true;
+    // }
+    // else{
+    //     alert("imported type is not a number");
+    //     return false
+    // }
+}
